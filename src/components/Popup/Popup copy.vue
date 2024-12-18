6e2255147be3f85367b0ec7f4cd5d744593e8deb
charl56 <charles.pichou56@gmail.com>
2024-12-18 20:41:57 +0100

    Popup bien

--- src/components/Popup/Popup copy.vue
+++ src/components/Popup/Popup copy.vue
@@ -1,4 +1,5 @@
 <template>
+    <!-- Inspiration => gallery : https://www.youtube.com/watch?v=v0UoqZJRP5M -->
     <!-- Inspiration => colors : https://victor.work/ -->
 
     <div class="popup-div">
@@ -32,6 +33,11 @@
             <div v-if="project" class="modal-content text-title">
                 <p v-html="project.name"></p>
             </div>
+
+            <div class="preview-img">
+                <img src="" alt=""> <!-- Preview image when hover img -->
+            </div>
+            <div class="gallery"></div>
 
             <div v-if="project" v-for="(info, index) in project.infos" class="modal-content text-infos">
                 <div class="modal-content__infos">
@@ -71,20 +77,17 @@ export default {
     },
     watch: {
         project() {
+            this.initGallery();
+
             // Wait DOM update
             nextTick(() => {
-
-                this.imagesWithIndex = null;
-
-                if (this.project != null) {
-                    this.imagesWithIndex = this.project.photos.map(photo => this.getImageUrl(photo.src));
-                }
                 this.initImagesAnimation();
             });
         }
     },
     data() {
         return {
+            rotationInterval: null,
             progress: 0,
             imagesWithIndex: null,
         }
@@ -133,6 +136,148 @@ export default {
 
 
         },
+        initGallery() {
+
+            if (this.project == null) {
+                return;
+            }
+
+            const gallery = document.querySelector('.gallery');
+            const previewImage = document.querySelector('.preview-img img');
+
+            // Clean elements
+            gallery.innerHTML = "";
+            previewImage.innerHTML = "";
+            previewImage.style.visibility = "hidden";
+
+            document.addEventListener("mousemove", function (e) {
+                const x = e.clientX;
+                const y = e.clientY;
+
+                const centerX = window.innerWidth / 2;
+                const centerY = window.innerHeight / 2;
+
+                const percentX = (x - centerX) / centerX;
+                const percentY = (y - centerY) / centerY;
+
+                const rotateX = 55 + percentY * 2;
+                const rotateY = percentX * 2;
+
+                gsap.to(gallery, {
+                    duration: 1,
+                    ease: "power2.out",
+                    rotateX: rotateX,
+                    rotateY: rotateY,
+                    overwrite: "auto"
+                });
+            });
+
+
+            this.imagesWithIndex = null;
+
+            if (this.project != null) {
+                this.imagesWithIndex = this.project.photos.map(photo => this.getImageUrl(photo.src));
+            }
+
+
+
+            for (let i = 0; i < 80; i++) {
+                const item = document.createElement('div');
+                item.classList.add('item');
+                item.className = 'item';
+                item.style.position = 'absolute';
+                item.style.top = '50%';
+                item.style.left = '50%';
+                item.style.transform = 'translate(-50%, -50%)';
+                item.style.width = '45px';
+                item.style.height = '60px';
+                item.style.margin = '10px';
+                item.style.transformStyle = 'preserve-3d';
+                item.style.background = '#b0b0b0';
+                // item.style.boxShadow = 'rgb(255 255 255) 15px -10px 25px -5px';
+
+                const img = document.createElement('img');
+                img.style.width = '100%';
+                img.style.height = '100%';
+                img.style.objectFit = 'cover';
+
+                if (this.imagesWithIndex && this.imagesWithIndex.length > 0) {
+                    img.src = this.imagesWithIndex[i % this.imagesWithIndex.length]
+                }
+
+
+                item.appendChild(img);
+                gallery.appendChild(item);
+            }
+
+            const items = document.querySelectorAll('.item');
+            const numberOfItems = items.length;
+            const angleIncrement = 360 / numberOfItems;
+
+
+            items.forEach((item, index) => {
+                gsap.set(item, {
+                    rotationY: 90,
+                    rotationZ: index * angleIncrement - 90,
+                    transformOrigin: "50% 300px"
+                });
+
+                item.addEventListener("mouseover", () => {
+                    const imgInsideItem = item.querySelector('img');
+                    previewImage.src = imgInsideItem.src;
+                    previewImage.style.visibility = "visible";
+
+                    clearInterval(this.rotationInterval);
+
+                    gsap.to(item, {
+                        x: 10,
+                        y: 10,
+                        z: 10,
+                        ease: "power2.out",
+                        duration: 0.5
+                    });
+                });
+
+                item.addEventListener("mouseout", () => {
+                    previewImage.style.visibility = "hidden";
+
+                    this.initRotationInterval();
+
+                    gsap.to(item, {
+                        x: 0,
+                        y: 0,
+                        z: 0,
+                        ease: "power2.out",
+                        duration: 0.5
+                    });
+
+                });
+            });
+            this.initRotationInterval();
+        },
+        initRotationInterval() {
+            this.rotationInterval = setInterval(() => {
+                const items = document.querySelectorAll('.item');
+
+                this.progress += 0.001;
+
+                const rotationProgress = this.progress * 360;        // O to 1
+                const numberOfItems = items.length;             // Up, set to 80 
+                const angleIncrement = 360 / numberOfItems;     // 4.5
+
+
+                items.forEach((item, index) => {
+                    // Current angle ~ degres where to go, from same origin to every one
+                    const currentAngle = index * angleIncrement - 90 + rotationProgress;
+
+                    gsap.to(item, {
+                        rotateZ: currentAngle,
+                        duration: 1,
+                        overwrite: "auto"
+                    });
+                });
+            }, 50);
+        },
         getImageUrl(src) {
             return import.meta.env.DEV
                 ? new URL(`../../../images/${src}`, import.meta.url).href
@@ -150,6 +295,19 @@ export default {
         },
         closePopup() {
             this.$emit('close');
+
+            const gallery = document.querySelector('.gallery');
+            const previewImage = document.querySelector('.preview-img img');
+
+            gallery.innerHTML = "";
+            previewImage.innerHTML = "";
+            previewImage.style.visibility = "hidden";
+            previewImage.style.height = "50vh";
+
+
+            // Clear rotation vars
+            clearInterval(this.rotationInterval);
+            this.progress = 0;
         }
     }
 }
@@ -213,10 +371,35 @@ export default {
     flex-direction: column;
     justify-content: flex-start;
 }
-
 /* ************************************* */
 
+.preview-img {
+    /* position: relative; */
+    /* top: 5vh; */
 
+    height: 95vh;
+    width: 100%;
+    z-index: 0;
+
+    img {
+        height: auto;
+        width: 70vw;
+
+        position: relative;
+        left: 15vw;
+
+        object-fit: cover;
+    }
+}
+
+.gallery {
+    /* position: absolute; */
+    z-index: 10;
+    top: 22.5%;
+    left: 50%;
+    transform-style: preserve-3d;
+    transform: translateX(-50%) rotateX(55deg);
+}
 
 /* ************************************* */
 .modal-content {
